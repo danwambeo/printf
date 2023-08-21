537bfb42624a061e914e10ad1d8cf5d06a33a0d0
ngarigrace <nyambu0105@gmail.com>
2023-08-21 02:09:56 -0700

    update

--- tasksizedg.c
+++ tasksizedg.c
@@ -11,18 +11,14 @@ int curr_a = *i + 1;
 int size = 0;
 
 if (format[curr_a] == 'l')
-{
 size = S_LONG;
-}
 else if (format[curr_a] == 'h')
-{
 size = S_SHORT;
-}
+
 if (size == 0)
-{
 *i = curr_a - 1;
-}
 else
 *i = curr_a;
+
 return (size);
 }
